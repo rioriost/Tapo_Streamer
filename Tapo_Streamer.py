57f442856562a84e44ab51bd6a0bc492d9e276a7
Rio Fujita <rio_github@rio.st>
2024-07-30 10:14:30 +0900

    Update Tapo_Streamer.py

--- Tapo_Streamer.py
+++ Tapo_Streamer.py
@@ -1,39 +1,45 @@
-#!/usr/bin/env python3
+#!/opt/homebrew/bin/python3.11
 
 # standard library
 import configparser
+import datetime
 import ipaddress
 import os
-import sys
 import pathlib
+import re
+import socket
+import sys
+import time
+
+cur_python = f"python{sys.version_info[0]}.{sys.version_info[1]}"
 
 # third party library
 try:
     import PySimpleGUI as sg
 except ModuleNotFoundError:
     try:
-        os.system('python3 -m pip install pysimplegui')
+        os.system(f'{cur_python} -m pip install pysimplegui')
         import PySimpleGUI as sg
     except ModuleNotFoundError: # pip doesn't resolve dependency, so we have to install it with brew
-        os.system('brew install tcl-tk')
+        os.system('brew install tcl-tk python-tk@3.11')
         import PySimpleGUI as sg
 
 try:
     import rtsp
 except ModuleNotFoundError:
-    os.system('python3 -m pip install rtsp')
+    os.system(f'{cur_python} -m pip install rtsp')
     import rtsp
 
 try:
     from PIL import Image, ImageTk
 except ModuleNotFoundError:
-    os.system('python3 -m pip install pillow')
+    os.system(f'{cur_python} -m pip install pillow')
     from PIL import Image, ImageTk
 
 try:
     import keyring
 except ModuleNotFoundError:
-    os.system('python3 -m pip install keyring')
+    os.system(f'{cur_python} -m pip install keyring')
     import keyring
 
 # Application Name
@@ -44,23 +50,35 @@ def open_window(user_id: str, user_pw: str, row_col: dict, hosts:dict) -> None:
     layout = [
         [[sg.Image(filename='', key="image-{}".format(hosts[f"stream{row}-{col}"])) for col in range(row_col["col"])] for row in range(row_col["row"])],
         [sg.Button('Connect', size=(10, 1), key ='-connect-'),
-         sg.Button('Disconnect', size=(10, 1), key = '-disconnect-')]
+         sg.Button('Disconnect', size=(10, 1), key = '-disconnect-'),
+         sg.Button('Restart', size=(10, 1), key = '-restart-')]
          ]
     is_streaming = False
-    window = sg.Window('Tapo Camera', layout, location=(32, 32), finalize=True, element_justification='left', font='Helvetica 18')
+    window = sg.Window('Tapo Camera', layout, location=(32, 32), finalize=True, resizable=True, element_justification='left', font='Helvetica 18', enable_close_attempted_event=True)
     clients = {f"{host}": None for host in hosts.values() if host != ""}
-    rtsp_urls = {f"{host}": f"rtsp://{user_id}:{user_pw}@{host}:554/stream2" for host in hosts.values() if host != ""}
+    rtsp_urls = {f"{host}": f"rtsp://{user_id}:{user_pw}@{host}/stream2" for host in hosts.values() if host != ""}
+    bootup = True
+    uptime = datetime.datetime.now()
     while True:
         event, values = window.read(timeout=1)
         if event == sg.WIN_CLOSED:
             break
-        elif event == "-connect-":
+        elif (datetime.datetime.now() - uptime).seconds >= 3600:
+            if is_streaming is True:
+                uptime = datetime.datetime.now()
+                for host in hosts.values():
+                    if host != "":
+                        print(f"Reconnecting to {host}...")
+                        clients[host].close()
+                        clients[host] = rtsp.Client(rtsp_server_uri=rtsp_urls[host], verbose=False)
+        elif event == "-connect-" or bootup is True:
             if is_streaming is False:
                 for host in hosts.values():
                     if host != "":
                         print(f"Connecting to {host}...")
-                        clients[host] = rtsp.Client(rtsp_server_uri=rtsp_urls[host])
+                        clients[host] = rtsp.Client(rtsp_server_uri=rtsp_urls[host], verbose=False)
                         is_streaming = True
+                bootup = False
         elif event == "-disconnect-":
             if is_streaming is True:
                 is_streaming = False
@@ -71,6 +89,8 @@ def open_window(user_id: str, user_pw: str, row_col: dict, hosts:dict) -> None:
                         clients[host] = None
                         img = Image.new('RGB', (640, 480), color=0)
                         window[f"image-{host}"].update(data=ImageTk.PhotoImage(img))
+        elif event == "-restart-": # restart the app for clear memory leak
+            os.execv(sys.argv[0], sys.argv)
         if is_streaming is True:
             for host in hosts.values():
                 if host != "":
@@ -79,19 +99,28 @@ def open_window(user_id: str, user_pw: str, row_col: dict, hosts:dict) -> None:
                         window[f"image-{host}"].update(data=ImageTk.PhotoImage(frame))
                     except:
                         print(f"Reconnecting to {host}...")
-                        clients[host] = rtsp.Client(rtsp_server_uri=rtsp_urls[host])
+                        clients[host] = rtsp.Client(rtsp_server_uri=rtsp_urls[host], verbose=False)
+        time.sleep(0.15)
     window.close()
 
-def validate_ip(ip: str) -> bool:
-    if ip is None:
+def validate_ip(ip_hostname: str) -> bool:
+    if ip_hostname is None:
         return False
-    if ip == "": # empty string is valid
+    if ip_hostname == "": # empty string is valid
         return True
-    try:
-        ip_obj = ipaddress.ip_address(ip)
-        return True
-    except ValueError:
-        return False
+    if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$", ip_hostname) is True: # maybe IP address
+        try:
+            ip_obj = ipaddress.ip_address(ip_hostname.split(":")[0])
+            return True
+        except ValueError:
+            return False
+    else: # maybe hostname
+        try:
+            ip = socket.gethostbyname(ip_hostname.split(":")[0])
+            if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip) is True:
+                return True
+        except:
+            return False
 
 def input_number(prompt: str) -> int:
     num = 0
@@ -127,10 +156,10 @@ def init_hosts() -> None:
                 for key, host in hosts.items():
                     tmp_ip = None
                     while validate_ip(tmp_ip) is False:
-                        tmp_ip = input_ip_address(f"IP address for {key}: ")
+                        tmp_ip = input_ip_address(f"IP address:port or hostname:port for {key} e.g. 192.168.1.2:554: ")
                     hosts[key] = tmp_ip
                 if sum(v == "" for v in hosts.values()) == row_num * col_num:
-                    print("Please enter at least one host IP address.")
+                    print("Please enter at least one host IP address or hostname.")
                 else:
                     break
         except KeyboardInterrupt:
